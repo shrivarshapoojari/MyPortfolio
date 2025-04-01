5d8a70341484f935d12969537f97ebe20e222643
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-01 14:19:58 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Home from './components/Home/Home'
 import './index.css'
 import './style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './components/Navbar'
  const App = () => {
    return (
       <BrowserRouter>
+      <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>} />
           
--- src/components/Home/Type.jsx
+++ src/components/Home/Type.jsx
@@ -1,30 +1,4 @@
-// import React from "react";
-// import TypeIt from "typeit-react";
-
-// function Type() {
-//   return (
-//     <TypeIt
-//       options={{
-//         strings: [
-//           "MERN Stack Developer",
-//           "Spring Boot Developer",
-//           "Full Stack Developer",
-//           "Problem Solver",
-//           "AI Enthusiast",
-//           "Curious & Passionate Learner"
-//         ],
-//         speed: 100,
-//         breakLines: false,
-//         loop: true,
-//         cursor: true,
-//         waitUntilVisible: true,
-//         nextStringDelay: 1500,
-//       }}
-      
-//     />
-//   );
-// }
-
+ 
 // export default Type;
 
  
