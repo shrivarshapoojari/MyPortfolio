cb3138063fdea17fb86f471dfa5876ade72a4022
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-05 16:14:43 +0530

    preloader updated

--- src/components/Pre.jsx
+++ src/components/Pre.jsx
@@ -1,6 +1,19 @@
+// import React from "react";
+// function Pre(props) {
+//   return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+// }
+
+// export default Pre;
+
+
 import React from "react";
+import pre from "../assets/pre.svg"
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={props.load ? "preloader" : "preloader-none"}>
+      <img src={pre}  alt="Loading..." />
+    </div>
+  );
 }
 
 export default Pre;
