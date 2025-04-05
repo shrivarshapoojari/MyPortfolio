c66c9ffacdb4bf4c6c648391e6cd567b67d294a3
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-04-05 15:54:48 +0530

    preloading animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
@@ -12,10 +12,25 @@ import About from './components/About/About'
 import Footer from './components/Footer'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact'
+import ScrollToTop from "./components/ScrollToTop";
+import Preloader from "../src/components/Pre";
+import { useEffect } from 'react'
  const App = () => {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
    return (
       <BrowserRouter>
+       <Preloader load={load} />
+       <div className="App" id={load ? "no-scroll" : "scroll"}>
       <NavBar/>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About />} />
@@ -23,6 +38,7 @@ import Contact from './components/Contact'
           <Route path="/contact" element= {<Contact/>}/>
         </Routes>
         <Footer/>
+        </div>
       </BrowserRouter>
    )
  }
